30101aa6996bd42079bbe7fa02f0637400965818
David E. Weirich <weirich.david@gmail.com>
2020-07-24 18:53:27 -0400

    filter in sql

--- app.py
+++ app.py
@@ -12,21 +12,31 @@ engine = sqlalchemy.create_engine('sqlite:///resident_schedule.db')
 con = engine.connect()
 
 
-schedule_data = pd.read_sql(con=con, sql='select * from schedule', parse_dates=['start_date', 'end_date'])
+query = """
+select 
+    PGY,
+    Name, 
+    Rotation, 
+    end_date
+from schedule
+where
+DATE('now') between start_date and end_date
+"""
+
+
+schedule_data = pd.read_sql(con=con, sql=query, parse_dates=['end_date'])
+schedule_data['Until'] = schedule_data['end_date'].dt.strftime('%b %d')
+schedule_data = schedule_data.drop('end_date', axis=1)
 
 
 @app.route("/", methods=["GET"])
 def home():
     global schedule_data
 
-    day = pd.Timestamp.today()
-
     result = ""
     for g, df in schedule_data.groupby('PGY'):
-        today = df[(df['start_date'] <= day) & (day <= df['end_date'])]
-
         result += f'<H1>PGY {g}</h1>'
-        result += today[['Name', 'Rotation', 'end_date']].rename({'end_date': 'Until'}, axis=1).to_html(index=False)
+        result += df.to_html(index=False)
         result += '\n'
 
     return result
