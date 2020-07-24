b77d9cb0cfd0f5043f725a484f8c824d05bcabbd
David E. Weirich <weirich.david@gmail.com>
2020-07-24 18:43:29 -0400

    Filtering on dates

--- app.py
+++ app.py
@@ -12,16 +12,24 @@ engine = sqlalchemy.create_engine('sqlite:///resident_schedule.db')
 con = engine.connect()
 
 
-df = pd.read_sql(con=con, sql='select * from schedule')
-
-print(df)
+schedule_data = pd.read_sql(con=con, sql='select * from schedule', parse_dates=['start_date', 'end_date'])
 
 
 @app.route("/", methods=["GET"])
 def home():
-    global df
+    global schedule_data
 
-    return df.to_html()
+    day = pd.Timestamp.today()
+
+    result = ""
+    for g, df in schedule_data.groupby('PGY'):
+        today = df[(df['start_date'] <= day) & (day <= df['end_date'])]
+
+        result += f'<H1>PGY {g}</h1>'
+        result += today[['Name', 'Rotation', 'end_date']].rename({'end_date': 'Until'}, axis=1).to_html(index=False)
+        result += '\n'
+
+    return result
 
 
 if __name__ == "__main__":
