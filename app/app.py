01382c8c5fdf75bcd0165080105a7092610801b9
David E. Weirich <weirich.david@gmail.com>
2020-08-05 22:40:22 -0400

    First given date page

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pandas as pd
 import sqlalchemy
 from flask import Flask
@@ -7,34 +9,56 @@ from flask import render_template
 app = Flask(__name__)
 
 
-engine = sqlalchemy.create_engine("sqlite:///resident_schedule.db")
-con = engine.connect()
+def get_data_from_date(date: Optional[str] = None) -> str:
+    if date is None:
+        date = "now"
+
+    query = f"""
+    select
+        PGY,
+        Name,
+        Rotation,
+        start_date,
+        end_date
+    from schedule
+    where
+    DATE('{date}') between DATE(start_date) and DATE(end_date)
+    """
+
+    engine = sqlalchemy.create_engine("sqlite:///resident_schedule.db")
+    con = engine.connect()
+
+    schedule_data = pd.read_sql(
+        con=con, sql=query, parse_dates=["start_date", "end_date"]
+    )
+    schedule_data["Starting"] = schedule_data["start_date"].dt.strftime("%B %d")
+    schedule_data["Until"] = schedule_data["end_date"].dt.strftime("%B %d")
+
+    return schedule_data
 
 
-query = """
-select
-    PGY,
-    Name,
-    Rotation,
-    end_date
-from schedule
-where
-DATE('now') between DATE(start_date) and DATE(end_date)
-"""
-
-
-schedule_data = pd.read_sql(con=con, sql=query, parse_dates=["end_date"])
-schedule_data["Until"] = schedule_data["end_date"].dt.strftime("%B %d")
+def prepare_table(df):
+    return df.drop(["PGY", "start_date", "end_date"], axis=1).to_html(
+        index=False, classes=["table", "table-striped"]
+    )
 
 
 @app.route("/", methods=["GET"])
 def home():
-    global schedule_data
+    schedule_data = get_data_from_date()
 
-    def prepare_table(df):
-        return df.drop(["PGY", "end_date"], axis=1).to_html(
-            index=False, classes=["table", "table-striped"]
-        )
+    groups = [
+        {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
+    ]
+
+    return render_template("templates.html", groups=groups)
+
+
+@app.route("/date/<year>/<month>/<day>/", methods=["GET"])
+def date_page(year=2021, month=1, day=1):
+    date = f"{int(year)}-{int(month):02}-{int(day):02}"
+
+    schedule_data = get_data_from_date(date)
 
     groups = [
         {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
