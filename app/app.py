080bfd7b98f06fe7c22f2ff66e308d1020a6f48d
David E. Weirich <weirich.david@gmailcom>
2024-06-19 10:51:41 -0400

    Finish page, strip spaces

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-templates = Jinja2Templates(directory='app/templates')
+templates = Jinja2Templates(directory="app/templates")
 
 
 def get_connection():
@@ -103,77 +103,110 @@ async def home(request: Request):
         {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
     ]
 
-    return templates.TemplateResponse("home.html", {"request": request, "groups": groups, "header_text": "Current Schedule for Today"})
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "groups": groups,
+            "header_text": "Current Schedule for Today",
+        },
+    )
 
 
 @app.get("/date/", response_class=HTMLResponse)
-async def date_page(request: Request, date: str = Query(..., description="Date parameter")):
+async def date_page(
+    request: Request, date: str = Query(..., description="Date parameter")
+):
     schedule_data = get_data_from_date(date)
-    
+
     groups = [
         {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
     ]
-    
-    return templates.TemplateResponse("home.html", {"request": request, "groups": groups, "header_text": f"Schedule for Date: {date}"})
 
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "groups": groups,
+            "header_text": f"Schedule for Date: {date}",
+        },
+    )
 
 
 @app.get("/date_picker/", response_class=HTMLResponse)
 def date_picker(request: Request):
     return templates.TemplateResponse("date_picker.html", {"request": request})
 
-# 
-# @app.route("/rotation/", methods=["GET"])
-# def rotation_schedule():
-#     rotation = request.args.get("rotation", type=str)
-# 
-#     schedule_data = get_rotation_schedule(rotation)
-# 
-#     groups = [
-#         {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
-#     ]
-# 
-#     return render_template(
-#         "home.html", groups=groups, header_text=f"Schedule for Rotation: {rotation}"
-#     )
-# 
-# 
-# @app.route("/resident/", methods=["GET"])
-# def resident_schedule():
-#     name = request.args.get("name", type=str)
-# 
-#     schedule_data = get_resident_schedule(name)
-# 
-#     groups = [
-#         {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
-#     ]
-# 
-#     return render_template(
-#         "home.html", groups=groups, header_text=f"Full Schedule for {name}"
-#     )
-# 
-# 
-# @app.route("/rotation_picker/", methods=["GET"])
-# def rotation_picker():
-#     rotation_list = get_all_rotation_names()
-# 
-#     return render_template(
-#         "rotation_picker.html", rotations=rotation_list["rotation"].to_list()
-#     )
-# 
-# 
-# @app.route("/resident_picker/", methods=["GET"])
-# def resident_picker():
-#     resident_list = get_all_resident_names()
-# 
-#     names = {
-#         f"pgy{pgy}": resident_list[resident_list["PGY"] == str(pgy)]["name"].to_list()
-#         for pgy in range(1, 6)
-#     }
-# 
-#     return render_template("resident_picker.html", names=names)
-# 
-# 
-# 
-# 
-# 
+
+@app.get("/rotation/", response_class=HTMLResponse)
+def rotation_schedule(
+    request: Request, rotation: str = Query(..., description="Rotation parameter")
+):
+    schedule_data = get_rotation_schedule(rotation)
+
+    groups = [
+        {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
+    ]
+
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "groups": groups,
+            "header_text": f"Schedule for Rotation: {rotation}",
+        },
+    )
+
+
+@app.get("/resident/", response_class=HTMLResponse)
+def resident_schedule(request: Request, name: str = Query(..., description="Name parameter")):
+    schedule_data = get_resident_schedule(name)
+
+    groups = [
+        {"df": prepare_table(df), "pgy": g} for g, df in schedule_data.groupby("PGY")
+    ]
+
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "groups": groups,
+            "header_text": f"Schedule for Resident: {name}",
+        },
+    )
+
+
+@app.get("/rotation_picker/", response_class=HTMLResponse)
+def rotation_picker(request: Request):
+    rotation_list = get_all_rotation_names()["rotation"].to_list()
+
+    return templates.TemplateResponse(
+        "rotation_picker.html",
+        {
+            "request": request,
+            "rotations": rotation_list,
+        },
+    )
+
+
+@app.route("/resident_picker/", methods=["GET"])
+def resident_picker(request: Request):
+    resident_list = get_all_resident_names()
+
+    name_list = {
+        f"pgy{pgy}": resident_list[resident_list["PGY"] == str(pgy)]["name"].to_list()
+        for pgy in range(1, 6)
+    }
+
+    return templates.TemplateResponse(
+        "resident_picker.html",
+        {
+            "request": request,
+            "names": name_list,
+        },
+    )
+
+
+
+
+
